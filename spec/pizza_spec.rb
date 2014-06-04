fa7b97214c94e390178994f87f8c17b312779bd8
Jered McCullough <mathias.armstrong@gmail.com>
2014-06-04 16:47:42 -0700

    I added a test to see if the pizza initializes with cheese when no other toppings are added

--- spec/pizza_spec.rb
+++ spec/pizza_spec.rb
@@ -7,13 +7,20 @@ describe Pizza do
 
   describe ".initialize" do
     it "records all of the toppings" do
-    toppings= [
-      Topping.new("mushrooms", vegetarian: true),
-      Topping.new("pepperoni")
-    ]
-    pizza = Pizza.new(toppings)
+      toppings= [
+        Topping.new("mushrooms", vegetarian: true),
+        Topping.new("pepperoni")
+      ]
+      pizza = Pizza.new(toppings)
 
-    expect(pizza.toppings).to eq(toppings)
+      expect(pizza.toppings).to eq(toppings)
+    end
+
+    it 'defaults the toppings to cheese only, if the pizza has no toppings' do
+      pizza = Pizza.new
+
+      expect(pizza.toppings.size).to eq(1)
+      expect(pizza.toppings.first.name).to eq('cheese')
     end
   end
 end
