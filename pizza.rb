e52a43bf8ef6a44540f0e115288d67ce09a2bf43
Jered McCullough <mathias.armstrong@gmail.com>
2014-06-04 13:56:49 -0700

    I added a attr_accessor to my pizza.rb in order to call the name. I also defined and initialize function that takes the name of the topping

--- pizza.rb
+++ pizza.rb
@@ -2,4 +2,12 @@ class Pizza
 end
 
 class Topping
+  attr_accessor :name
+
+  # instantiate a new  topping.
+  # name - the string name of the topping
+  # and returns a new topping object
+  def initialize(name)
+    @name = name
+  end
 end
